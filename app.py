77d03d75477dbb9586c5baf9be7c23c70039040a
Mikhail Ryabchikov <Nolifegonnamad@gmail.com>
2025-09-07 16:02:28 +0400

    initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Моя первая игра!")
+
+player_x = 350
+player_y = 450
+player_width = 50
+player_height = 50
+player_speed = 0.1
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player_x += player_speed
+    if keys[pygame.K_UP]:
+        player_y -= player_speed
+    if keys[pygame.K_DOWN]:
+        player_y += player_speed
+
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, RED, (player_x, player_y, player_width, player_height))
+    pygame.display.flip()
+
+pygame.quit()
